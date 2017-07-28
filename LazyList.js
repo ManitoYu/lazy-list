e0b7bad5ad8b2476bea13c123fc37f9a2b6f3e68
ManitoYu <412950798@qq.com>
2017-07-28 10:55:39 +0800

    update

--- LazyList.js
+++ LazyList.js
@@ -2,37 +2,26 @@ class Cons {
 	constructor(headThunk, tailThunk) {
 		this.head = headThunk
 		this.tail = tailThunk
-
-		this.ops = []
 	}
 
 	toList() {
-		var self = this
 		function go(cons, acc) {
 			if (!cons.head) return acc
-			cons = self.ops.reduce((c, f) => f(c.head(), c.tail()), cons)
 			return go(cons.tail(), [cons.head(), ...acc])
 		}
 		return go(this, []).reverse()
 	}
 
 	map(f) {
-		this.ops.push((h, t) => new Cons(() => f(h), () => t))
-		// return this.foldRight(() => new Cons(null, null))((h, t) => new Cons(() => f(h), () => t))
-		return this
+		return this.foldRight(() => new Cons(null, null))((h, t) => new Cons(constant(f(h)), t))
 	}
 
 	filter(f) {
-		this.ops.push((h, t) => f(h) ? new Cons(() => h, () => t) : t)
-		// return this.foldRight(() => new Cons(null, null))((h, t) => f(h) ? new Cons(() => h, () => t) : t)
-		return this
+		return this.foldRight(() => new Cons(null, null))((h, t) => f(h) ? new Cons(() => h, t) : t())
 	}
 
 	foldRight(z) {
-		return f => {
-			if (!this.head) return z()
-			return f(this.head(), this.tail().foldRight(z)(f))
-		}
+		return f => !this.head ? z() : f(this.head(), () => this.tail().foldRight(z)(f))
 	}
 }
 
@@ -58,10 +47,12 @@ function tail(arr) {
 	return arr.slice(1)
 }
 
-const list = LazyList.from([1, 2, 3, 4, 5, 6])
-	.map(x => console.log('1') || 1)
-	.map(x => console.log('2') || 2)
-	.map(x => console.log('3') || 3)
-	.toList()
+function constant(value) {
+	return () => value
+}
 
-console.log(list)
+const list = LazyList.from([1, 2, 3])
+	.map(x => console.log(1) || x)
+	.map(x => console.log(2) || x)
+	.map(x => console.log(3) || x)
+	.toList()
